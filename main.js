c5a63bbe61d96f2aa4cb8f492de077cce41f273a
Victor Onyebuchi <victorchike247@gmail.com>
2021-05-13 06:00:31 +0100

    added button handlers

---
+++ main.js
@@ -0,0 +1,36 @@
+const sessionMins = 25;
+const breakMins = 5;
+
+let initialSessionMins = sessionMins;
+let initialBreakMins = breakMins;
+
+const  max = 60; 
+const min = 1;
+
+const breakLength = document.getElementById('break-length');
+const sessionLength = document.getElementById('session-length');
+const timeLeft = document.getElementById('time-left');
+
+document.getElementById('reset').addEventListener('click', () => {
+
+})
+
+document.getElementById('start_stop').addEventListener('click', () => {
+    
+})
+
+document.getElementById('break-decrement').addEventListener('click', () => {
+    
+})
+
+document.getElementById('break-increment').addEventListener('click', () => {
+    
+})
+
+document.getElementById('session-decrement').addEventListener('click', () => {
+    
+})
+
+document.getElementById('session-increment').addEventListener('click', () => {
+    
+})
